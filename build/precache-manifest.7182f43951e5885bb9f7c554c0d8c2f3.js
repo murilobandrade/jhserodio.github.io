ff2457fa60baa0c55daef4a48b6ad3f21aa8976c
jhserodio <joao.serodio@vnator.com>
2020-09-11 15:52:57 -0300

    feat: update build

---
+++ build/precache-manifest.7182f43951e5885bb9f7c554c0d8c2f3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1835d1399221100e9e2877ca1242293",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "ae4499233bf41dfd2f94",
+    "url": "/index.html/static/css/main.2103f7c2.chunk.css"
+  },
+  {
+    "revision": "115fc7fe182ea3427038",
+    "url": "/index.html/static/js/2.50f4a800.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/index.html/static/js/2.50f4a800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4499233bf41dfd2f94",
+    "url": "/index.html/static/js/main.c1428603.chunk.js"
+  },
+  {
+    "revision": "fc850299a20ad7ee9d34",
+    "url": "/index.html/static/js/runtime-main.0a511a5d.js"
+  },
+  {
+    "revision": "c05cfcea66591be5dac8e1b9c3223264",
+    "url": "/index.html/static/media/eu.c05cfcea.jpeg"
+  },
+  {
+    "revision": "010b604f84431eb7cdbc6620aa64cb55",
+    "url": "/index.html/static/media/firacode-bold.010b604f.woff2"
+  },
+  {
+    "revision": "22cf339e034bb9a3d21c0f7c6c11fade",
+    "url": "/index.html/static/media/firacode-bold.22cf339e.woff"
+  },
+  {
+    "revision": "f49305f3ff86f0f33e04658fa795c95c",
+    "url": "/index.html/static/media/firacode-bold.f49305f3.otf"
+  },
+  {
+    "revision": "004ca30540f4e610737895585f14a6b5",
+    "url": "/index.html/static/media/firacode-light.004ca305.woff"
+  },
+  {
+    "revision": "bc4d306ac11284a7d5460b426f3ff739",
+    "url": "/index.html/static/media/firacode-light.bc4d306a.otf"
+  },
+  {
+    "revision": "f8f87b9826c9d68e6666cc483bd0451c",
+    "url": "/index.html/static/media/firacode-light.f8f87b98.woff2"
+  },
+  {
+    "revision": "551a04f3a6544dcb9468992a3d87a2b0",
+    "url": "/index.html/static/media/firacode-medium.551a04f3.woff"
+  },
+  {
+    "revision": "7064d44fb7d3128f4958b8b907ef1d83",
+    "url": "/index.html/static/media/firacode-medium.7064d44f.otf"
+  },
+  {
+    "revision": "f18fd1b3cc4fc5265dc28c68947294a4",
+    "url": "/index.html/static/media/firacode-medium.f18fd1b3.woff2"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae5cb0e02d548d7177ac1e73855a4cc9.js"
+  "/index.html/precache-manifest.7182f43951e5885bb9f7c554c0d8c2f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
