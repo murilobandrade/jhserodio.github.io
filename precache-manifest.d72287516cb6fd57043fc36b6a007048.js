7685e6b1dd65bf9d2f346fb8370a0d08157d23f4
Murilo Borges de Andrade <murilo.b.andrade@gmail.com>
2020-09-14 23:24:54 -0300

    Updates

---
+++ precache-manifest.d72287516cb6fd57043fc36b6a007048.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74981729d9bd9a1d2dcaad640e23587d",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "2d5983e4e6b980c9b732",
+    "url": "/index.html/static/css/main.6b32f3b9.chunk.css"
+  },
+  {
+    "revision": "6a51a3902deddd7649dc",
+    "url": "/index.html/static/js/2.9f411b93.chunk.js"
+  },
+  {
+    "revision": "2d5983e4e6b980c9b732",
+    "url": "/index.html/static/js/main.b91e271e.chunk.js"
+  },
+  {
+    "revision": "8b0c016663b53bb047b5",
+    "url": "/index.html/static/js/runtime-main.4d477ab4.js"
+  },
+  {
+    "revision": "0524767a5d901e97d71bd62602d43a4e",
+    "url": "/index.html/static/media/eu.0524767a.jpeg"
+  },
+  {
+    "revision": "010b604f84431eb7cdbc6620aa64cb55",
+    "url": "/index.html/static/media/firacode-bold.010b604f.woff2"
+  },
+  {
+    "revision": "22cf339e034bb9a3d21c0f7c6c11fade",
+    "url": "/index.html/static/media/firacode-bold.22cf339e.woff"
+  },
+  {
+    "revision": "f49305f3ff86f0f33e04658fa795c95c",
+    "url": "/index.html/static/media/firacode-bold.f49305f3.otf"
+  },
+  {
+    "revision": "004ca30540f4e610737895585f14a6b5",
+    "url": "/index.html/static/media/firacode-light.004ca305.woff"
+  },
+  {
+    "revision": "bc4d306ac11284a7d5460b426f3ff739",
+    "url": "/index.html/static/media/firacode-light.bc4d306a.otf"
+  },
+  {
+    "revision": "f8f87b9826c9d68e6666cc483bd0451c",
+    "url": "/index.html/static/media/firacode-light.f8f87b98.woff2"
+  },
+  {
+    "revision": "551a04f3a6544dcb9468992a3d87a2b0",
+    "url": "/index.html/static/media/firacode-medium.551a04f3.woff"
+  },
+  {
+    "revision": "7064d44fb7d3128f4958b8b907ef1d83",
+    "url": "/index.html/static/media/firacode-medium.7064d44f.otf"
+  },
+  {
+    "revision": "f18fd1b3cc4fc5265dc28c68947294a4",
+    "url": "/index.html/static/media/firacode-medium.f18fd1b3.woff2"
+  }
+]);
