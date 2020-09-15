9b4500c03cb1e690a06bc8b1425b3e14e35de8ea
Murilo Borges de Andrade <murilo.b.andrade@gmail.com>
2020-09-14 23:38:46 -0300

    Updates

---
+++ precache-manifest.e20022b5515a7fda3226f818a9057580.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7411cec1136a37200bb2b5f1356f42e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef7031e20b195b60ad5",
+    "url": "/static/css/main.7353a188.chunk.css"
+  },
+  {
+    "revision": "3d358edceffb9855a8bb",
+    "url": "/static/js/2.1d0c1e7f.chunk.js"
+  },
+  {
+    "revision": "aef7031e20b195b60ad5",
+    "url": "/static/js/main.df67ab08.chunk.js"
+  },
+  {
+    "revision": "595a508e15c85949a698",
+    "url": "/static/js/runtime-main.7e561a75.js"
+  },
+  {
+    "revision": "0524767a5d901e97d71bd62602d43a4e",
+    "url": "/static/media/eu.0524767a.jpeg"
+  },
+  {
+    "revision": "010b604f84431eb7cdbc6620aa64cb55",
+    "url": "/static/media/firacode-bold.010b604f.woff2"
+  },
+  {
+    "revision": "22cf339e034bb9a3d21c0f7c6c11fade",
+    "url": "/static/media/firacode-bold.22cf339e.woff"
+  },
+  {
+    "revision": "f49305f3ff86f0f33e04658fa795c95c",
+    "url": "/static/media/firacode-bold.f49305f3.otf"
+  },
+  {
+    "revision": "004ca30540f4e610737895585f14a6b5",
+    "url": "/static/media/firacode-light.004ca305.woff"
+  },
+  {
+    "revision": "bc4d306ac11284a7d5460b426f3ff739",
+    "url": "/static/media/firacode-light.bc4d306a.otf"
+  },
+  {
+    "revision": "f8f87b9826c9d68e6666cc483bd0451c",
+    "url": "/static/media/firacode-light.f8f87b98.woff2"
+  },
+  {
+    "revision": "551a04f3a6544dcb9468992a3d87a2b0",
+    "url": "/static/media/firacode-medium.551a04f3.woff"
+  },
+  {
+    "revision": "7064d44fb7d3128f4958b8b907ef1d83",
+    "url": "/static/media/firacode-medium.7064d44f.otf"
+  },
+  {
+    "revision": "f18fd1b3cc4fc5265dc28c68947294a4",
+    "url": "/static/media/firacode-medium.f18fd1b3.woff2"
+  }
+]);
